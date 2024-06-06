8a71c5e70169cde286d09d9150662604b62395dc
Andrew <sorce@live.co.za>
2024-06-06 18:06:57 +0200

    Initial commit

---
+++ WCFServiceApp/App_Code/IPatientService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IPatientService
+    {
+
+        [OperationContract]
+        [WebGet]
+        //string GetData(int value);
+        List<Patient> GetAllPatients();
+
+        //[OperationContract]
+        //CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
---
+++ WCFServiceApp/App_Code/PatientService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class PatientService : IPatientService
+{
+    public List<Patient> GetAllPatients()
+    {
+        try
+        {
+            using (var _contextDb = new DataEntities())
+            {
+                return _contextDb.Patients.ToList();
+            }
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
+}
